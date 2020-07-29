5fd29c71e54af3fecb8a5468a027a5549ec44a6d
Wagmore <853869@cognizant.com>
2020-07-29 18:04:02 +0530

    Updates

---
+++ precache-manifest.f966970cf18e305c77a35ad85064c949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10183af100546ad89d87ce3d607c0f92",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "bfde4ce7392088f9f3d7",
+    "url": "/Weather-app/static/css/main.953f9d14.chunk.css"
+  },
+  {
+    "revision": "fe24eee9d54d6eeda49e",
+    "url": "/Weather-app/static/js/2.17abd280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.17abd280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfde4ce7392088f9f3d7",
+    "url": "/Weather-app/static/js/main.aa7a96b9.chunk.js"
+  },
+  {
+    "revision": "a2861103e0194e8c138c",
+    "url": "/Weather-app/static/js/runtime-main.1d012fff.js"
+  }
+]);
