3c770b0d77b93486bb329b22312e142868fb41a1
Wagmore <853869@cognizant.com>
2020-07-29 18:05:44 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import Axios from 'axios';
+import './App.css';
+import DisplayWeather from './components/DisplayWeather.js';
+import Navbar from './components/Navbar.js';
+class App extends React.Component {
+
+//state
+state = {
+  coords:{
+    latitude:45,
+    longitude:60
+  },
+  data: {},
+  inputData:""
+}
+
+  componentDidMount(){
+    //get device location
+    if(navigator.geolocation){
+      
+      navigator.geolocation.getCurrentPosition((position) =>{
+        let newCoords = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+
+        this.setState({coords:newCoords});
+      //Api call
+      Axios.get(`http://api.weatherstack.com/current?access_key=f42e385dec93cac11347df62b4cd6429&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res =>{
+        
+      let weatherData = {
+        location:res.data.location.name,
+        temperature: res.data.current.temperature,
+        description: res.data.current.weather_descriptions[0],
+        region: res.data.location.region,
+        country: res.data.location.country,
+        wind_speed: res.data.current.wind_speed,
+        pressure: res.data.current.pressure,
+        precip: res.data.current.precip,
+        humidity: res.data.current.humidity,
+        img: res.data.current.weather_icons
+      }
+
+      this.setState({ data:weatherData });
+
+
+      })
+      } )
+    }else{
+      console.log("Not Supported")
+    }
+  }
+
+  //track the input field
+  change =(value) =>{
+    this.setState({ inputData: value })
+  }
+
+  changeWeather= (event) =>{
+    event.preventDefault();
+ 
+//api call
+Axios.get(`http://api.weatherstack.com/current?access_key=f42e385dec93cac11347df62b4cd6429&query=${this.state.inputData}`).then(res =>{
+  let weatherData = {
+    location:res.data.location.name,
+    temperature: res.data.current.temperature,
+    description: res.data.current.weather_descriptions[0],
+    region: res.data.location.region,
+    country: res.data.location.country,
+    wind_speed: res.data.current.wind_speed,
+    pressure: res.data.current.pressure,
+    precip: res.data.current.precip,
+    humidity: res.data.current.humidity,
+    img: res.data.current.weather_icons
+  }
+
+  this.setState({ data:weatherData });
+})
+  }
+
+  render(){
+  return (
+    <div className="App">
+      <div className="container">
+        <Navbar changeWeather = {this.changeWeather} changeRegion= {this.change}/>
+      <DisplayWeather weatherData= {this.state.data}/>  
+      <h2 className="footer">Made By Harish Wagmore</h2>  
+      </div> 
+      </div>
+  );
+}
+}
+export default App;
